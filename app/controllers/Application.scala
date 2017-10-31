b8d8f7785aad6db383e7ed5a2b2da96e99175039
eelfonik <kinoflee@gmail.com>
2017-10-31 23:17:56 +0100

    combine 2 apis' response using `for comprehension`

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,8 +4,10 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
-import java.util.Date
-import java.text.SimpleDateFormat
+import java.time.{ZoneId, ZonedDateTime}
+import java.time.format.DateTimeFormatter
+// import java.util.Date
+// import java.text.SimpleDateFormat
 import scala.concurrent.Future
 import play.api.libs.ws._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -18,16 +20,31 @@ class Application @Inject() (components: ControllerComponents, ws: WSClient)
   def index = Action.async {
     // note here we get a Future of response with ws
     val responseF = ws.url("http://api.sunrise-sunset.org/json?" + "lat=31.005&lng=121.4086111&formatted=0").get()
-    // map is a buildin method of Future class
-    responseF.map { res =>
+    val weatherF = ws.url("http://api.openweathermap.org/data/2.5/weather?lat=31.005&lon=121.4086111&units=metric&appid=188c545b513123ecd39468edf04c097b").get()
+
+    // as we have 2 apis, use for comprehension instead of flatMap/map
+    // 之前还在想如果用了两个单独的map，要如何变成一个,塞进view.html里
+
+    for {
+      res <- responseF
+      weather <- weatherF
+    } yield {
       val json = res.json
       // 这里这个奇怪的\符号是什么， 为什么不用. ？
       // 然后这个as[String]是哪里来的？
       val sunRiseTimeStr = (json \ "results" \ "sunrise").as[String]
       val sunSetTimeStr = (json \ "results" \ "sunset").as[String]
-      val sunInfo = SunInfo(sunRiseTimeStr, sunSetTimeStr)
-      Ok(views.html.index(sunInfo))
+      val sunRiseTime = ZonedDateTime.parse(sunRiseTimeStr)
+      val sunSetTime = ZonedDateTime.parse(sunSetTimeStr)
+      val formatter = DateTimeFormatter.ofPattern("HH:mm:ss").withZone(ZoneId.of("Asia/Shanghai"))
+      val sunInfo = SunInfo(sunRiseTime.format(formatter), sunSetTime.format(formatter))
+
+      val weatherJson = weather.json
+      val temp = (weatherJson \ "main" \ "temp").as[Double]
+
+      Ok(views.html.index(sunInfo, temp)) 
     }
+    
     // val time = new Date()
     // val timeStr = new SimpleDateFormat().format(time)
 
