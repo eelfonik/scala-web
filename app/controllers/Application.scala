e7b4c6092dc20ac02acc53af8a7277662d3ef646
eelfonik <kinoflee@gmail.com>
2017-10-31 11:27:14 +0100

    add model package & use WSClient to get info from outer API
    - note the use of `case class` here

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,17 +7,34 @@ import play.api.mvc._
 import java.util.Date
 import java.text.SimpleDateFormat
 import scala.concurrent.Future
+import play.api.libs.ws._
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class Application @Inject() (components: ControllerComponents)
+import model.SunInfo
+
+class Application @Inject() (components: ControllerComponents, ws: WSClient)
     extends AbstractController(components) {
   // note the .async here   
   def index = Action.async {
-    val time = new Date()
-    val timeStr = new SimpleDateFormat().format(time)
+    // note here we get a Future of response with ws
+    val responseF = ws.url("http://api.sunrise-sunset.org/json?" + "lat=31.005&lng=121.4086111&formatted=0").get()
+    // map is a buildin method of Future class
+    responseF.map { res =>
+      val json = res.json
+      // 这里这个奇怪的\符号是什么， 为什么不用. ？
+      // 然后这个as[String]是哪里来的？
+      val sunRiseTimeStr = (json \ "results" \ "sunrise").as[String]
+      val sunSetTimeStr = (json \ "results" \ "sunset").as[String]
+      val sunInfo = SunInfo(sunRiseTimeStr, sunSetTimeStr)
+      Ok(views.html.index(sunInfo))
+    }
+    // val time = new Date()
+    // val timeStr = new SimpleDateFormat().format(time)
+
     // this line use successful method of Future
     // So you don't have specific the ExecutionContext implictly
     // in this method, it simply wraps the result of an expression in a Future
     // without scheduling the computation in a different thread
-    Future.successful {Ok(views.html.index(timeStr))}  
+    // Future.successful { Ok(views.html.index(timeStr)) }  
   }
 }
---
+++ app/models/SunInfo.scala
@@ -0,0 +1,3 @@
+package model
+
+case class SunInfo(sunRise: String, sunSet: String)
